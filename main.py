90ddfc943dce0b3c2b577ce7124e3e04ea35340a
ghost-mann <iamaob8@protonmail.com>
2025-05-08 13:10:59 +0300

    add html and python file

---
+++ main.py
@@ -0,0 +1,22 @@
+Lorem ipsum dolor sit amet. Ut itaque voluptatem 33 autem similique et ipsam numquam vel aspernatur nemo. Ex sint animi ad minima fugit qui quia quis aut quas deleniti et rerum culpa et soluta autem!
+
+Ut consectetur aliquam 33 iste vero quo quia veniam est ullam dolorum aut aliquid autem et amet aspernatur. 33 odit sint qui odit architecto qui porro beatae cum internos molestiae 33 officiis rerum. Et dolorem alias eos impedit voluptas aut beatae magnam ab libero Quis et accusantium autem.
+
+Ab quas iusto et dolore quidem 33 illo tenetur et temporibus ipsum. Rem veritatis facilis sed unde quaerat ea obcaecati delectus hic enim esse. Ab impedit animi non incidunt excepturi aut enim minus ut officiis distinctio qui sequi odit vel fugit eveniet ea rerum adipisci.
+Lorem ipsum dolor sit amet. Ut itaque voluptatem 33 autem similique et ipsam numquam vel aspernatur nemo. Ex sint animi ad minima fugit qui quia quis aut quas deleniti et rerum culpa et soluta autem!
+
+Ut consectetur aliquam 33 iste vero quo quia veniam est ullam dolorum aut aliquid autem et amet aspernatur. 33 odit sint qui odit architecto qui porro beatae cum internos molestiae 33 officiis rerum. Et dolorem alias eos impedit voluptas aut beatae magnam ab libero Quis et accusantium autem.
+
+Ab quas iusto et dolore quidem 33 illo tenetur et temporibus ipsum. Rem veritatis facilis sed unde quaerat ea obcaecati delectus hic enim esse. Ab impedit animi non incidunt excepturi aut enim minus ut officiis distinctio qui sequi odit vel fugit eveniet ea rerum adipisci.Lorem ipsum dolor sit amet. Ut itaque voluptatem 33 autem similique et ipsam numquam vel aspernatur nemo. Ex sint animi ad minima fugit qui quia quis aut quas deleniti et rerum culpa et soluta autem!
+
+Ut consectetur aliquam 33 iste vero quo quia veniam est ullam dolorum aut aliquid autem et amet aspernatur. 33 odit sint qui odit architecto qui porro beatae cum internos molestiae 33 officiis rerum. Et dolorem alias eos impedit voluptas aut beatae magnam ab libero Quis et accusantium autem.
+
+Ab quas iusto et dolore quidem 33 illo tenetur et temporibus ipsum. Rem veritatis facilis sed unde quaerat ea obcaecati delectus hic enim esse. Ab impedit animi non incidunt excepturi aut enim minus ut officiis distinctio qui sequi odit vel fugit eveniet ea rerum adipisci.Lorem ipsum dolor sit amet. Ut itaque voluptatem 33 autem similique et ipsam numquam vel aspernatur nemo. Ex sint animi ad minima fugit qui quia quis aut quas deleniti et rerum culpa et soluta autem!
+
+Ut consectetur aliquam 33 iste vero quo quia veniam est ullam dolorum aut aliquid autem et amet aspernatur. 33 odit sint qui odit architecto qui porro beatae cum internos molestiae 33 officiis rerum. Et dolorem alias eos impedit voluptas aut beatae magnam ab libero Quis et accusantium autem.
+
+Ab quas iusto et dolore quidem 33 illo tenetur et temporibus ipsum. Rem veritatis facilis sed unde quaerat ea obcaecati delectus hic enim esse. Ab impedit animi non incidunt excepturi aut enim minus ut officiis distinctio qui sequi odit vel fugit eveniet ea rerum adipisci.Lorem ipsum dolor sit amet. Ut itaque voluptatem 33 autem similique et ipsam numquam vel aspernatur nemo. Ex sint animi ad minima fugit qui quia quis aut quas deleniti et rerum culpa et soluta autem!
+
+Ut consectetur aliquam 33 iste vero quo quia veniam est ullam dolorum aut aliquid autem et amet aspernatur. 33 odit sint qui odit architecto qui porro beatae cum internos molestiae 33 officiis rerum. Et dolorem alias eos impedit voluptas aut beatae magnam ab libero Quis et accusantium autem.
+
+Ab quas iusto et dolore quidem 33 illo tenetur et temporibus ipsum. Rem veritatis facilis sed unde quaerat ea obcaecati delectus hic enim esse. Ab impedit animi non incidunt excepturi aut enim minus ut officiis distinctio qui sequi odit vel fugit eveniet ea rerum adipisci.
